f250965ce3d90112677275e63dd9b90e89ee295f
zytan98 <zheng.yuan83@gmail.com>
2023-06-13 22:44:36 +0800

    updated fetch process

--- encyclopedia/encyclopedia/spiders/geturl.py
+++ encyclopedia/encyclopedia/spiders/geturl.py
@@ -1,6 +1,7 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-import scrapy
+import base64
+from bs4 import BeautifulSoup
 
 class GeturlSpider(CrawlSpider):
     name = "geturl"
@@ -12,7 +13,17 @@ class GeturlSpider(CrawlSpider):
     )
 
     def parse(self, response):
-        yield{
-            'url':response.url
+        b64 = base64.b64encode(response.body)
+        sb64 = b64.decode()
+        meta_data = {
+            "url": response.url,
+            "title": response.xpath("//title/text()").extract_first(),
+            "description": response.xpath('//meta[@name="description"]/@content').get(),
+            "abstract": response.xpath('//meta[@name="abstract"]/@content').get(),
+            "content": BeautifulSoup(response.body).get_text(),
+            "html": sb64 
+            #"content": response.xpath("//div[contains(@class, 'doccontentwrapper')]/p").getall(),   #Just to get an estimate of the JSON file size with content of pages
+            # Add more meta information as needed
         }
+        yield meta_data
 
