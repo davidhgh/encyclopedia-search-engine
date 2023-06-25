efb3eaead11b932d93c3a46b19e7a4f0e9665032
deadqazw <goldmaster135@gmail.com>
2023-06-25 13:51:40 +0800

    Add index records

--- indexer/src/main/java/com/lucene/encyclopedia/App.java
+++ indexer/src/main/java/com/lucene/encyclopedia/App.java
@@ -1,4 +1,15 @@
 package com.lucene.encyclopedia;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 
 /**
  * Hello world!
@@ -8,6 +19,41 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! + App.java" );
+        
+        
+        try {
+            // Step 1: Create a Lucene Directory
+            Directory directory = FSDirectory.open(Paths.get("C:\\Users\\junji\\Desktop\\jajaja\\lucene\\indexs"));
+
+            // Step 2: Create an IndexWriter
+            Analyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            IndexWriter indexWriter = new IndexWriter(directory, config);
+
+            // Step 3: Create Documents
+            Document doc1 = new Document();
+            doc1.add(new Field("ID", "12345678", TextField.TYPE_STORED));
+            doc1.add(new Field("title", "Document 1", TextField.TYPE_STORED));
+            doc1.add(new Field("content", "This is the content of document 1", TextField.TYPE_STORED));
+            
+            Document doc2 = new Document();
+            doc1.add(new Field("ID", "87654321", TextField.TYPE_STORED));
+            doc2.add(new Field("title", "Document 2", TextField.TYPE_STORED));
+            doc2.add(new Field("content", "This is the content of document 2", TextField.TYPE_STORED));
+
+            // Step 4: Add Documents to Index
+            indexWriter.addDocument(doc1);
+            indexWriter.addDocument(doc2);
+
+            // Step 5: Commit or Optimize the Index
+            indexWriter.commit(); // or indexWriter.forceMerge(1);
+
+            // Step 6: Close the IndexWriter
+            indexWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ indexer/src/main/java/com/lucene/encyclopedia/Retrieve.java
@@ -0,0 +1,63 @@
+package com.lucene.encyclopedia;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.document.Document;
+import java.nio.file.Paths;
+public class Retrieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println( "Hello World! + test.java" );
+        
+        
+        
+        String indexPath = "C:\\Users\\junji\\Desktop\\jajaja\\lucene\\indexs";
+
+        try {
+            Directory directory = FSDirectory.open(Paths.get(indexPath));
+            IndexReader reader = DirectoryReader.open(directory);
+
+            // Perform search or retrieve documents using the reader
+            myMethod(reader);
+            
+            
+            reader.close();
+            directory.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+  static void myMethod(IndexReader reader) {
+
+	    try {
+		    int numDocs = reader.numDocs();
+
+		    for (int i = 0; i < numDocs; i++) {
+		        Document doc = reader.document(i);
+		        
+		        // Access and process the document fields, Case-sensitive
+		        String id = doc.get("ID");
+		        String title = doc.get("title");
+		        String content = doc.get("content");
+		        
+		        // Perform further processing with the document
+		        System.out.println("Document ID: " + id);
+		        System.out.println("Title: " + title);
+		        System.out.println("Content: " + content);
+		        System.out.println("-------------------------------------");
+		    }
+		} catch (Exception e) {
+            e.printStackTrace();
+		}
+
+  	}
+  
+  
+  
+  
+  
+  
+  
+}
